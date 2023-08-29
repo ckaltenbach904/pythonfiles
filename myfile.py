5131fe9958953c6a2172db71a994c1ffc7359e3d
catharina904 <catharina904@outlook.com>
2023-08-29 13:53:05 +0200

    python learning!

---
+++ myfile.py
@@ -0,0 +1,82 @@
+print("Hello future Python programmer!");\
+print("Python is a great language");\
+print(2. ** 3.)
+print(2 ** 3)
+print(13 / 4 + 13 % 4)
+print(10 - 6 ** 2 / 9 * 10 + 1)
+print(2*(2+3))
+print(10 / 2)
+print(6. // 4)
+x = 10 / 4
+y = 5 / 2.0
+print (x + y)
+print(2 * 3 ** 3 * 4)
+print(2 ** 3.)
+
+#variables
+amount_of_apples = 2
+cost_of_apple = 5
+print(amount_of_apples * cost_of_apple);\
+
+# cost 2 increased by 2 = 4 multiply by 4 = 16
+amount = 4
+cost = 2
+cost += 2
+print(amount * cost);\
+
+# 22 divide by 2 = 12; 12 + 44 = 55.0
+age = 22
+AGE = 44
+age /= 2
+print(age + AGE);\
+
+# strings has higher prio than int
+y = 5
+y = "Jack"
+print(y);\
+
+#NAME ERROR (TypeError: unsupported operand type(s) for +: 'float' and 'str')
+
+#x = 5
+#y = 6
+#z = 7
+#print(x+y+z)
+
+#print("Hello, jack!")
+print("Hello, Sally!");\
+
+
+#print Sally#123
+name = "Sally"# employee name
+ 
+data = "#123" 
+print (name+data);\
+
+#input
+favorite_color = ("What is your favorite color?")
+print("Your favorite color is " + favorite_color);\
+
+#input
+
+age =input("My age is: " )
+print (age);\
+
+# Hello Python would be the output
+
+inputString = input('Enter a string: ')
+print(inputString, sep='#', end='&')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
